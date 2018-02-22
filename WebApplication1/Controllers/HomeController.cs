1aba10d1fc6e9d254cd56d0c348003856026ea4f
JayaniDaluwatte <jayanidaluwatte@gmail.com>
2018-02-22 11:20:14 +0530

    third commit

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace WebApplication1.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application home page";
+            ViewBag.Message = "Home page";
 
             return View();
         }
